ab2055d85f83ad3196fabdada303c6fd199d0e13
Daniel <joonhee.u@gmail.com>
2023-12-10 23:00:36 +0900

    🌱 Add: NextJS 템플릿 추가

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main className="p-6 flex min-h-screen flex-col items-center justify-center gap-8">
+      <h1 className="text-6xl sm:text-9xl">Hello?</h1>
+      <div className="p-6 rounded-xl bg-gray-200 dark:bg-gray-900">
+        <p className="max-w-screen-sm">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores amet,
+          repellat culpa totam, in ea, neque saepe eum enim voluptate aut cumque
+          maxime mollitia nostrum perferendis omnis soluta excepturi quod?
+        </p>
+      </div>
+    </main>
+  )
+}
